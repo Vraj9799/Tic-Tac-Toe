09054e006645e6b6b33bf4ccf7e773b42b4ce930
Vraj Shah <vrajshah363@gmail.com>
2023-02-01 09:48:10 +0530

    Initial Project

---
+++ src/main/java/com/lld/App.java
@@ -0,0 +1,13 @@
+package com.lld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Tic Tac Toe !!!" );
+    }
+}
